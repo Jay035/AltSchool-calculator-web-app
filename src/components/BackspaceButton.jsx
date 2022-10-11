d1dd0dbe08f80958522e2fa8a438a490ef377fbd
Codexjay <jaydencolins08@gmail.com>
2022-10-11 12:20:06 +0100

    added backspace icon

--- src/components/BackspaceButton.jsx
+++ src/components/BackspaceButton.jsx
@@ -1,12 +1,13 @@
 import { useContext } from "react";
 import { NumberContext } from "./NumberProvider";
+import backspaceIcon from '../backspace.png';
 
 export default function BackspaceButton({ label }) {
   const { handleBackspaceButton } = useContext(NumberContext);
 
   return (
     <button type="button" className="Button" onClick={handleBackspaceButton}>
-      {label}
+      <img src={backspaceIcon} alt="backspace icon" />
     </button>
   );
 }
